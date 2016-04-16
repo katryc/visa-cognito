ea95c8a00899ca0a0689c9f6e2eb3e9a52526315
kat markava <katryc@outlook.com>
2016-04-16 16:26:33 -0400

    added landing page

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160416193056) do
+ActiveRecord::Schema.define(version: 20160416193409) do
 
   create_table "users", force: :cascade do |t|
-    t.integer  "phone"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.string   "phone_number"
